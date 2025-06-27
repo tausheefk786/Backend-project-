1b2cda9a7243ce6e6e2e23f9b70e98a1b6af725f
k786 <tausheefk786@gmail.com>
2025-06-27 21:40:59 +0530

    utils code discussion

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieparser from "cookie-parser";
+
+
+const app =  express();
+
+app.use(cors({
+    origin: process.env.cors_origin,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true,limit:
+    "16kb"
+}))
+
+app.use(express.static("public"))
+app.use(cookieparser())
+
+
+export {app};
+// .then(()=>{
+//     app.listen(process.env.PORT || 8000 ,()=>{
+//         console.log(`server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch(err)=>{
+//     console.log("MONGO db connection failed !!!",err);
+// }
---
+++ src/utils/apierror.js
@@ -0,0 +1,25 @@
+class apierror extends Error{
+    constructor(
+        statuscode,
+        message="something went wrong",
+        error =[],
+        stack =""
+    ){
+        super(message)
+        this.statuscode=statuscode
+        this.data = null
+        this.message= message
+        this.success = false;
+        this.errors = this.errors
+
+        if(stack){
+            this.stack= stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+
+
+}
+
+export {apierror};
